41d3e5c57f3ef91ea1056b6fa07537b1de756018
vamsisrinivasraavi <94818117+vamsisrinivasraavi@users.noreply.github.com>
2021-11-22 09:46:13 +0530

    Add files via upload

---
+++ 2a.c
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Distance 
+{
+private:
+int meter;
+friend int addfive(Distance a);
+public:
+Dostance():meter(0)
+{
+}
+};
+int addfive(Distance d)
+{
+5;
+return d.meter;
+}
+int main()
+{
+Distance D;
+cout<<"Distance:"<<addfive(D)'
+return 0;
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
